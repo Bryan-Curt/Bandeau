6ce40b7dd80b76a62e83f8ae5b40031a92e5db74
rnuma <rnuma@cst-tp-b10205>
2018-12-11 17:18:29 +0100

    ajout class zoom

--- src/main/java/bandeau/Zoom.java
+++ src/main/java/bandeau/Zoom.java
@@ -1,18 +1,18 @@
-import java.awt.Font;
-
 package bandeau;
+
+import java.awt.Font;
 
 public class Zoom extends Effect {
     
-     public Zoom(){
-        for (int i = 5; i < 60 ; i+=5) {
+     public Zoom(){ }
+     
+     public void launchEffect(Bandeau myBandeau){
+                for (int i = 5; i < 60 ; i+=5) {
 		myBandeau.setFont(new Font(message, Font.ITALIC, 5+i));
 		myBandeau.sleep(100);
 	}
-     }
+                myBandeau.setMessage(message);
      
-     public void launchEffect(Bandeau myBandeau){
-         myBandeau.setMessage(message);
      }
 }
  
