0a40612f85dbab62bf9d1030e6276f846e5a45bf
bcurt <bcurt@cst-tp-b10206>
2018-12-11 17:52:46 +0100

    debug scenario

--- src/main/java/bandeau/Zoom.java
+++ src/main/java/bandeau/Zoom.java
@@ -4,16 +4,18 @@ import java.awt.Font;
 
 public class Zoom extends Effect {
     
-     public Zoom(){ 
+     public Zoom(String text){ 
      super();
+     message=text;
              }
      
      public void launchEffect(Bandeau myBandeau){
+                myBandeau.setMessage(message);
+
                 for (int i = 5; i < 60 ; i+=5) {
 		myBandeau.setFont(new Font(message, Font.ITALIC, 5+i));
 		myBandeau.sleep(100);
-	}
-                myBandeau.setMessage(message);
+                }
      
      }
 }
