07e6c387018c999b80d2e8f7b4b61aa5db7b5d22
bcurt <bcurt@cst-tp-b10206>
2018-12-11 17:54:42 +0100

    debug textRotation

--- src/main/java/bandeau/TextRotation.java
+++ src/main/java/bandeau/TextRotation.java
@@ -2,15 +2,16 @@ package bandeau;
 
 public class TextRotation extends Effect {
     
-    public TextRotation(){
+    public TextRotation(String text){
         super();
+        message=text;
     }
     public void launchEffect(Bandeau myBandeau){
+        myBandeau.setMessage(message);
         for (int i = 0; i <= 100; i++) {
 		myBandeau.setRotation(2*Math.PI*i / 100);
 		myBandeau.sleep(100);
 	}
-        myBandeau.setMessage(message);
  
     }
 }
