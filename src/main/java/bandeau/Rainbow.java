490a260222daef8fb1f149fb7cb8251334d2c89d
roxan <roxan@DESKTOP-RA3NU26.home>
2018-12-14 11:18:37 +0100

    edit rainbow class

--- src/main/java/bandeau/Rainbow.java
+++ src/main/java/bandeau/Rainbow.java
@@ -1,7 +1,15 @@
 
 package bandeau;
 
+import java.awt.Color;
 
-public class Rainbow {
+public class Rainbow extends Effect {
+    public Rainbow(String text){
+        super();
+        message=text;
+    }
     
+    public void launchEffect(Bandeau myBandeau){
+    myBandeau.setMessage(message);
+    }
 }
