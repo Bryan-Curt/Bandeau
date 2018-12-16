fd650940639e6959d24027bf229a418c3ef25789
root <root@192.168.0.102>
2018-12-16 21:08:43 +0100

    Scenario complet

--- src/main/java/bandeau/Scenario.java
+++ src/main/java/bandeau/Scenario.java
@@ -13,20 +13,39 @@ import java.awt.Color;
  */
 public class Scenario {
     private final Bandeau myBandeau = new Bandeau();
-    
     public static void main(String[] args) throws InterruptedException {
 	    new Scenario().scenario();
     }
     
     
     public void scenario() throws InterruptedException{
-       String text="Oui";
-       new Rainbow(text).launchEffect(myBandeau,175);
-       String text2="Les couleurs de McDonald's !";
-       new BackgroundColor(text2).launchEffect(myBandeau);
-       new TextColor(text2).launchEffect(Color.YELLOW,myBandeau);
-       new Zoom(text2).launchEffect(myBandeau);
-       new TextRotation(text2).launchEffect(myBandeau);
+       String text="Bienvenue chez McDonald's";
+       int fontSize = 20;
+       String font = "Comic Sans MS";
+       new TextSettings(text,Color.YELLOW,font,fontSize).launchEffect(myBandeau);
+       new BackgroundColor(text,new Color(0,100,0)).launchEffect(myBandeau);
+       myBandeau.sleep(2000);
+       fontSize = new Zoom(text,font,fontSize,100).launchEffect(myBandeau);
+                       System.out.println(fontSize);
+
+       String text2="Venez comme vous êtes !";
+       new TextSettings(text2,Color.ORANGE,font,fontSize).launchEffect(myBandeau);
+       new Rainbow(text2).launchEffect(myBandeau,25);
+       
+       String text3="Avec la vraie couleur c'est mieux";
+       new BackgroundColor(text3,Color.RED).launchEffect(myBandeau);
+       new TextSettings(text3,Color.YELLOW,font,fontSize).launchEffect(myBandeau);
+       myBandeau.sleep(2000);
+       
+       
+       String text4="Mais venez quand même";
+       new TextSettings(text4,Color.YELLOW,font,fontSize).launchEffect(myBandeau);
+       new TextRotation(text4).launchEffect(myBandeau);
+
+       new TextSettings("PALAPA",Color.YELLOW,font,fontSize).launchEffect(myBandeau);
+       myBandeau.sleep(1000);
+       new TextSettings("PA  PA",Color.YELLOW,font,fontSize).launchEffect(myBandeau);
+
     }
     
 }
